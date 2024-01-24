356215f93ff8a41aa1480d65f91b53a64b6c6839
Davi Viana <89869115+davifv22@users.noreply.github.com>
2024-01-24 00:58:35 -0300

    Create Header e Menu
    - Header com logo, info e logout/config
    - Menu com collapse left
    - Implementação fontawesome

---
+++ frontend/src/app/pages/home/components/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent implements OnInit {
+
+
+  constructor( ) {
+   }
+
+   ngOnInit(): void {
+   }
+}
+
+
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,60 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { UserService } from '../../services/user.service';
-import { AuthService } from '../../services/auth.service';
-import { User } from '../../models/user'
-import { Router } from '@angular/router';
+import { MenuComponent } from "./components/menu/menu.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule, RouterLink],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [CommonModule, RouterLink, MenuComponent, HeaderComponent]
 })
 export class HomeComponent implements OnInit {
-  User:User
+  ngOnInit(): void {}
 
-  constructor(private userService: UserService, private authService: AuthService, private router: Router) {
-    this.User = {
-      id:0,
-      nome:'',
-      user:'',
-      email:'',
-      situacao:'',
-      is_admin:false,
-      api_key:''
-    }
-   }
 
-   ngOnInit(): void {
-    this.getUsuario();
-   }
+  constructor( ) { }
 
-   sair() {
-    if (this.authService.logout()) {
-      this.router.navigate(['login'])
-    }
-   }
-
-   getUsuario(){
-    this.userService.getUser().subscribe(
-      {
-        next: (res) => {
-          this.User = {
-            id: res.id,
-            nome: res.nome,
-            user: res.user,
-            email: res.email,
-            situacao: res.situacao,
-            is_admin: res.is_admin,
-            api_key: res.api_key
-          }
-        },
-        error: (err) => console.log('not found')
-      }
-    )
-   }
 }
-
