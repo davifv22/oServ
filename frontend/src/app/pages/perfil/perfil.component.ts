1348b1476d14cf210052e0cc342a29ff33ab033a
Davi Viana <89869115+davifv22@users.noreply.github.com>
2024-02-03 20:27:58 -0300

    Create frontend perfil

--- frontend/src/app/pages/perfil/perfil.component.ts
+++ frontend/src/app/pages/perfil/perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SubMenuComponent } from "../../components/sub-menu/sub-menu.component";
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-perfil',
@@ -12,9 +13,40 @@ import { CommonModule } from '@angular/common';
 export class PerfilComponent implements OnInit {
   title:string = ''
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.title = 'PERFIL'
   }
+
+
+  previewUrl: string | ArrayBuffer | null = null;
+
+onFileSelected(event: any) {
+  const file: File = event.target.files[0];
+  const reader = new FileReader();
+
+  // if (file) {
+  //   const formData = new FormData();
+  //   formData.append('file', file);
+
+  //   this.http.post('http://localhost:3000/api/upload', formData).subscribe(
+  //     (response) => {
+  //       console.log(response);
+  //     },
+  //     (error) => {
+  //       console.error('Erro ao enviar o arquivo:', error);
+  //     }
+  //   );
+  // }
+
+  reader.onload = (e: any) => {
+    this.previewUrl = e.target.result;
+  };
+
+  reader.readAsDataURL(file);
+}
+
+
+
 }
