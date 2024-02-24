03af900b2c3272909050857b05c8195920f7de1b
Davi Viana <89869115+davifv22@users.noreply.github.com>
2024-02-24 11:30:23 -0300

    add footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, MenuComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
