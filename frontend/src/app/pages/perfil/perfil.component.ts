c750a78edcbe40812c581f4de536dcc76c2baff6
Davi Viana <89869115+davifv22@users.noreply.github.com>
2024-03-19 13:41:11 -0300

    fix pagina perfil

--- frontend/src/app/pages/perfil/perfil.component.ts
+++ frontend/src/app/pages/perfil/perfil.component.ts
@@ -1,23 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { SubMenuComponent } from "../../components/sub-menu/sub-menu.component";
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../../models/user'
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [SubMenuComponent, CommonModule],
+  imports: [SubMenuComponent, CommonModule, FormsModule],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent implements OnInit {
   title:string = ''
+  User:User
 
-  constructor(private http: HttpClient) { }
+  constructor(private userService: UserService, private http: HttpClient) {
+    this.User = {
+      idUser:0,
+      nome:'',
+      user:'',
+      email:'',
+      situacao:'',
+      isAdmin:false,
+      apiKey:''
+    }
+  }
 
   ngOnInit(): void {
     this.title = 'PERFIL'
+    this.getUsuario();
   }
+
+  getUsuario(){
+    this.userService.getUser().subscribe(
+      {
+        next: (res) => {
+          this.User = {
+            idUser: res.idUser,
+            nome: res.nome,
+            user: res.user,
+            email: res.email,
+            situacao: res.situacao,
+            isAdmin: res.isAdmin,
+            apiKey: res.apiKey
+          }
+        },
+        error: (err) => console.log('not found')
+      }
+    )
+   }
 
 
   previewUrl: string | ArrayBuffer | null = null;
@@ -45,8 +79,5 @@ onFileSelected(event: any) {
   };
 
   reader.readAsDataURL(file);
-}
-
-
-
+ }
 }
