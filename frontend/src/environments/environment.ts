a2cf92002d893170c5989b26847351a50d623f9f
Davi Viana <davi_viana22@hotmail.com>
2024-06-04 13:39:03 -0300

    fix environment debug

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,14 @@
+import { DEBUG } from './debug'
+
 export const environment = {
-  production: true,
-  url: 'https://api.itsdevcode.com.br/api'
+  production: false,
+  url: ''
 };
+
+if (DEBUG) {
+  environment.production = false;
+  environment.url = 'http://localhost:5000/api'
+} else {
+  environment.production = true;
+  environment.url = 'https://api.itsdevcode.com.br/api'
+}
