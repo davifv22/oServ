5edccfe36341285ae84047845afc4fd07a2fa475
Davi Viana <89869115+davifv22@users.noreply.github.com>
2024-06-04 00:59:40 -0300

    fix mysql docker

--- backend/config.py
+++ backend/config.py
@@ -2,10 +2,12 @@ import socket
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'aplicacao_flask'
-USERNAME = 'oServ'
+USERNAME = 'root'
+DATABASE_PASSWORD = 'oServ'
 PASSWORD = '12345'
 #SERVER = f'{socket.gethostbyname(socket.gethostname())}'
-SERVER = '192.168.0.106'
+SERVER = 'mysql'
 PORT_MYSQL = '3306'
 DB = 'db'
-SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{SERVER}:{PORT_MYSQL}/{DB}'
+
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USERNAME}:{DATABASE_PASSWORD}@{SERVER}:{PORT_MYSQL}/{DB}'
