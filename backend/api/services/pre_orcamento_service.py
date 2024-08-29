f0b79788db250a1a6d9cfd97e91cf08e652b1034
Davi Viana <89869115+davifv22@users.noreply.github.com>
2024-08-29 03:09:07 -0300

    fix crud cadastro pre orçamento

--- backend/api/services/pre_orcamento_service.py
+++ backend/api/services/pre_orcamento_service.py
@@ -1,13 +1,19 @@
 from ..models import pre_orcamento_model
 from api.app import db
 
+def insert_preOrcamento(pre_orcamento):
+    pre_orcamento_bd = pre_orcamento_model.PreOrcamentoModel(idTipoRequerimento=pre_orcamento.idTipoRequerimento, idServico=pre_orcamento.idServico, valor=pre_orcamento.valor)
+    db.session.add(pre_orcamento_bd)
+    db.session.commit()
+    return pre_orcamento_bd
+
+def update_preOrcamento(idTipoRequerimento, pre_orcamento):
+    pass
 
 def get_preOrcamentos():
-    preOrcamento_bd = pre_orcamento_model.PreOrcamentoModel.query.all()
-    preOrcamento_list = [{
-        'idTipoRequerimento': preOrcamento.idTipoRequerimento,
-        'idServico':preOrcamento.idServico,
-        'valor':preOrcamento.valor}
-        for preOrcamento in preOrcamento_bd
-    ]
-    return preOrcamento_list
+    pre_orcamento_bd = pre_orcamento_model.PreOrcamentoModel.query.all()
+    return pre_orcamento_bd
+
+def get_preOrcamento_id(idTipoRequerimento):
+    pre_orcamento_bd = pre_orcamento_model.PreOrcamentoModel.query.filter_by(idTipoRequerimento=idTipoRequerimento).first()
+    return pre_orcamento_bd
--- frontend/src/app/services/cadastros/pre-orcamento.service.ts
+++ frontend/src/app/services/cadastros/pre-orcamento.service.ts
@@ -26,4 +26,15 @@ export class PreOrcamentoService {
     const params = { apiKey: `${apiKey}`, page: page, per_page: per_page, isPaginate: isPaginate};
     return this.http.get<any>(`${this.apiUrl}/pre_orcamentos`, { params });
   }
+
+  postPreOrcamento(json: any, id: number): Observable<PreOrcamento> {
+    let apiKey = sessionStorage.getItem('apiKey');
+
+    const params = { apiKey: `${apiKey}` };
+    if (id == 0) {
+      return this.http.post<any>(`${this.apiUrl}/pre_orcamentos`, json, { params });
+    } else {
+      return this.http.post<any>(`${this.apiUrl}/pre_orcamento/${id}`, json, { params });
+    }
+  }
 }
