6b3340158f6cb65f0ceebd4f081da649f3c4c24b
Davi Viana <89869115+davifv22@users.noreply.github.com>
2024-08-29 01:36:00 -0300

    fix crud tipo requerimento

--- frontend/src/app/services/cadastros/tipo-requerimento.service.ts
+++ frontend/src/app/services/cadastros/tipo-requerimento.service.ts
@@ -26,6 +26,17 @@ export class TipoRequerimentoService {
     let apiKey = sessionStorage.getItem('apiKey');
 
     const params = { apiKey: `${apiKey}`, page: page, per_page: per_page, isPaginate: isPaginate};
-    return this.http.get<any>(`${this.apiUrl}/tipo_requerimento`, { params });
+    return this.http.get<any>(`${this.apiUrl}/tipo_requerimentos`, { params });
+  }
+
+  postTipoRequerimento(json: any, id: number): Observable<TipoRequerimento> {
+    let apiKey = sessionStorage.getItem('apiKey');
+
+    const params = { apiKey: `${apiKey}` };
+    if (id == 0) {
+      return this.http.post<any>(`${this.apiUrl}/tipo_requerimentos`, json, { params });
+    } else {
+      return this.http.post<any>(`${this.apiUrl}/tipo_requerimento/${id}`, json, { params });
+    }
   }
 }
