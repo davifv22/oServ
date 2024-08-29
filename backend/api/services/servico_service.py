7aeab976df6206d5640f939e15ca8669846aa5a6
Davi Viana <89869115+davifv22@users.noreply.github.com>
2024-08-29 01:54:04 -0300

    fix crud servicos

--- backend/api/services/servico_service.py
+++ backend/api/services/servico_service.py
@@ -1,15 +1,25 @@
 from ..models import servico_model
 from api.app import db
 
+def insert_servico(servico):
+    servico_bd = servico_model.ServicoModel(descricao=servico.descricao, tipo=servico.tipo, valor=servico.valor, situacao=servico.situacao)
+    db.session.add(servico_bd)
+    db.session.commit()
+    return servico_bd
+
+def update_servico(idServico, servico):
+    servico_model.ServicoModel.query.filter_by(idServico=idServico).update(
+            {"descricao": servico['descricao'], "tipo": servico['tipo'], "valor": servico['valor'], "situacao": servico['situacao']})
+
+    db.session.commit()
+    
+    servico_bd = get_servico_id(idServico)
+    return servico_bd
 
 def get_servicos():
-    servico_bd = servico_model.ServicoModel.query.all()
-    servico_list = [{
-        'idServico': servico.idServico,
-        'descricao':servico.descricao,
-        'tipo':servico.tipo, 
-        'valor':servico.valor,
-        'situacao':servico.situacao}
-        for servico in servico_bd
-    ]
-    return servico_list
+    servicos_bd = servico_model.ServicoModel.query.all()
+    return servicos_bd
+
+def get_servico_id(idServico):
+    servico_bd = servico_model.ServicoModel.query.filter_by(idServico=idServico).first()
+    return servico_bd
--- frontend/src/app/pages/cadastros/servicos/gerenciar-servicos/gerenciar-servicos.component.ts
+++ frontend/src/app/pages/cadastros/servicos/gerenciar-servicos/gerenciar-servicos.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { booleanAttribute, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card';
-import { MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatDialogRef } from '@angular/material/dialog'
+import { MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { ServicosService } from '../../../../services/cadastros/servicos.service';
+import { Servicos } from '../../../../models/servicos';
 
 @Component({
   selector: 'app-gerenciar-servicos',
@@ -21,13 +24,45 @@ import { MatSelectModule } from '@angular/material/select';
     MatDialogClose,
     MatCardModule,
     MatSelectModule,
+    CommonModule,
   ],
   templateUrl: './gerenciar-servicos.component.html',
   styleUrl: './gerenciar-servicos.component.css'
 })
 export class GerenciarServicosComponent {
-  constructor(
-    public modal: MatDialogRef<GerenciarServicosComponent>) {}
+  data:Servicos
+
+  constructor(public modal: MatDialogRef<GerenciarServicosComponent>, private servicosService: ServicosService) {
+    this.data = inject<Servicos>(MAT_DIALOG_DATA);
+  }
+
+  Servico(f: NgForm) {
+    const json = {
+      descricao: f.value['descricao'],
+      tipo: parseInt(f.value['tipo']),
+      valor: f.value['valor'],
+      situacao: booleanAttribute(f.value['situacao'])
+    }
+    if (this.data == null) {
+      this.servicosService.postServico(json, 0).subscribe(
+        (response) => {
+          return window.location.reload()
+        },
+        (error) => {
+          return error.message
+        }
+      )
+    } else {
+      this.servicosService.postServico(json, this.data.idServico).subscribe(
+        (response) => {
+          return window.location.reload()
+        },
+        (error) => {
+          return error.message
+        }
+      )
+    }
+  }
 
   onNoClick(): void {
     this.modal.close();
